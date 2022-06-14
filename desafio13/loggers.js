92f198ec9e4d1456bc5fbd7a26cecc0edc0d13f5
Tomás Aronna San Martín <tomasaronna@gmail.com>
2022-06-14 15:16:21 -0300

    Generé los loggers

---
+++ desafio13/loggers.js
@@ -0,0 +1,24 @@
+const log4js = require("log4js");
+const loggerConsola = log4js.getLogger("consola");
+const loggerWarn = log4js.getLogger("archivoWarning");
+const loggerError = log4js.getLogger("archivoError");
+
+log4js.configure({
+  appenders: {
+    loggerConsola: { type: "console" },
+    loggerWarning: { type: "file", filename: "warn.log" },
+    loggerError: { type: "file", filename: "error.log" },
+  },
+  categories: {
+    default: { appenders: ["loggerConsola"], level: "trace" },
+    consola: { appenders: ["loggerConsola"], level: "info" },
+    archivoWarning: {
+      appenders: ["loggerError", "loggerConsola"],
+      level: "error",
+    },
+    archivoError: {
+      appenders: ["loggerWarning", "loggerConsola"],
+      level: "warn",
+    },
+  },
+});
