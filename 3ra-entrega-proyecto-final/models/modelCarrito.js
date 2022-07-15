e40facec4049fa050e9dfa0c69eae729fde429d1
Tomás Aronna San Martín <tomasaronna@gmail.com>
2022-07-14 22:01:54 -0300

    Desarrollo del modelo

--- 3ra-entrega-proyecto-final/models/modelCarrito.js
+++ 3ra-entrega-proyecto-final/models/modelCarrito.js
@@ -1,1 +1,14 @@
 const mongoose = require("mongoose");
+
+const chartScheme = new mongoose.Schema({
+  idChart: { type: Number, require: true },
+  idProducto: { type: Number, require: true },
+  nombre: { type: String, require: true, max: 100 },
+  precio: { type: Number, require: true, max: 1000000 },
+  categoria: { type: String, require: true, max: 100 },
+  jugadores: { type: String, require: true, max: 100 },
+  totalProductos: { type: Number, require: true },
+  totalPrecio: { type: Number, require: true },
+});
+
+module.exports = ("carrito", chartScheme);
